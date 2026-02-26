b177c2daa4cb711fe1584a0bcb55fd0b5feda8c3
Suren Petrosyan <suren.k.petrosyan@gmail.com>
2026-02-26 17:01:17 +0200

    newtheme

--- content/index.ts
+++ content/index.ts
@@ -1,8 +1,9 @@
 import { travelTopic } from "./topics/travel"
 import { environmentTopic } from "./topics/environment"
+import { theatreTopic } from "./topics/theatre"
 import type { Topic } from "@/lib/types"
 
-export const topics: Topic[] = [travelTopic, environmentTopic]
+export const topics: Topic[] = [travelTopic, environmentTopic, theatreTopic]
 
 export function getTopicBySlug(slug: string): Topic | undefined {
   return topics.find((t) => t.slug === slug)
